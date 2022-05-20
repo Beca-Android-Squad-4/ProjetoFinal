bc766d4a93346f45b2ae819c0ad8c7758d9cd2a8
tpaivafe <theylonfeitosa@gmail.com>
2022-05-20 15:32:44 -0300

    Setup SplashScreen

--- app/src/main/java/com/example/projetofinalquad4/SplashActivity.kt
+++ app/src/main/java/com/example/projetofinalquad4/SplashActivity.kt
@@ -1,11 +1,52 @@
 package com.example.projetofinalquad4
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.view.WindowInsets
+import androidx.appcompat.app.AppCompatActivity
+import com.example.projetofinalquad4.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+    private val binding by lazy { ActivitySplashBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        setContentView(binding.root)
+
+        if (Build.VERSION.SDK_INT >= 30) {
+            binding.root.windowInsetsController?.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+        } else {
+            // Note that some of these constants are new as of API 16 (Jelly Bean)
+            // and API 19 (KitKat). It is safe to use them, as they are inlined
+            // at compile-time and do nothing on earlier devices.
+            binding.root.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LOW_PROFILE or
+                View.SYSTEM_UI_FLAG_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        }
+
+        val delay = 1000L
+        // installSplashScreen()
+        setContentView(binding.root)
+
+        // TODO Procurar uma forma mais atual
+        @Suppress("DEPRECATION")
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, delay)
+
+//        binding.root.viewTreeObserver.addOnDrawListener(object : ViewTreeObserver.OnDrawListener{
+//            override fun onDraw() {
+//                Thread.sleep(delay)
+//                binding.root.viewTreeObserver.removeOnDrawListener(this)
+//            }
+//
+//        })
     }
 }
