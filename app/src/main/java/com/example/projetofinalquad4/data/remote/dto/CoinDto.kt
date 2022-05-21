0982c78ededbbcf53288a8be72c8d1d70eb11493
tpaivafe <theylonfeitosa@gmail.com>
2022-05-21 11:49:11 -0300

    add mockCoinList

--- app/src/main/java/com/example/projetofinalquad4/data/remote/dto/CoinDto.kt
+++ app/src/main/java/com/example/projetofinalquad4/data/remote/dto/CoinDto.kt
@@ -1,7 +1,5 @@
 package com.example.projetofinalquad4.data.remote.dto
 
-import com.example.projetofinalquad4.model.Coin
-
 data class CoinDto(
     val id: String,
     val is_active: Boolean,
@@ -10,6 +8,27 @@ data class CoinDto(
     val rank: Int,
     val symbol: String,
     val type: String
+)
+
+fun mockCoinDto() = listOf(
+    CoinDto(
+        "4131",
+        true,
+        true,
+        "Bitcoin",
+        51,
+        "BTC",
+        "https://media.istockphoto.com/vectors/bitcoin-sign-icon-for-internet-money-crypto-currency-symbol-and-coin-vector-id912885640?k=20&m=912885640&s=170667a&w=0&h=mTXiKykLdQnEZ77HJ_IA85hpGODDi-Nh_1e6rP2WWe4=",
+    ),
+    CoinDto(
+        "4131",
+        true,
+        true,
+        "Metacoin",
+        531,
+        "MTC",
+        "https://s2.coinmarketcap.com/static/img/coins/200x200/6498.png",
+    )
 )
 
 fun CoinDto.toCoin(): Coin {
