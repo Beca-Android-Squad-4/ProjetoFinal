ca528e787e141c7f93d3cf468a8f55b2d039b7f7
tpaivafe <theylonfeitosa@gmail.com>
2022-05-21 11:45:47 -0300

    Setup UI in main fragment

--- app/src/main/java/com/example/projetofinalquad4/model/Coin.kt
+++
@@ -1,9 +0,0 @@
-package com.example.projetofinalquad4.model
-
-data class Coin(
-    val id: String,
-    val is_active: Boolean,
-    val name: String,
-    val rank: Int,
-    val symbol: String
-)
