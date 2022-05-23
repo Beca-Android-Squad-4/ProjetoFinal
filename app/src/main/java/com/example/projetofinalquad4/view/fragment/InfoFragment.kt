1fb5fbc5c42d486ec086ba354e933a4409d13e43
samuelbaldasso <samuelbaldasso93@gmail.com>
2022-05-23 10:29:36 -0300

    feat: new components for details

---
+++ app/src/main/java/com/example/projetofinalquad4/view/fragment/InfoFragment.kt
@@ -0,0 +1,32 @@
+package com.example.projetofinalquad4.view.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.projetofinalquad4.R
+import com.example.projetofinalquad4.viewModel.DetailsViewModel
+
+class InfoFragment : Fragment() {
+    companion object {
+        fun newInstance() = InfoFragment()
+    }
+
+    private lateinit var viewModel: DetailsViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.favorites_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(DetailsViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+}
