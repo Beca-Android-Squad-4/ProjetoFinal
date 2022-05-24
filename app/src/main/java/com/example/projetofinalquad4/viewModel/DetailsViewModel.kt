1f7a67d3ea201819ca79646e4b299a905d02de16
Ana Gomes Souza <agomesou@emeal.nttdata.com>
2022-05-24 16:08:54 -0300

    Desfazendo as alterações na MainViewModel e na DetailsViewModel

--- app/src/main/java/com/example/projetofinalquad4/viewModel/DetailsViewModel.kt
+++ app/src/main/java/com/example/projetofinalquad4/viewModel/DetailsViewModel.kt
@@ -1,37 +1,6 @@
 package com.example.projetofinalquad4.viewModel
 
+import androidx.lifecycle.ViewModel
 
-import androidx.lifecycle.*
-import com.example.projetofinalquad4.data.remote.dto.CoinApiResult
-import com.example.projetofinalquad4.data.remote.dto.CoinDto
-import com.example.projetofinalquad4.data.repository.CoinRepostitory
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import java.lang.Exception
 
-class DetailsViewModel (private val coinRepostitory: CoinRepostitory) : ViewModel() {
-    private val _coins = MutableLiveData<CoinApiResult<List<CoinDto>>>()
-    val coins: LiveData<CoinApiResult<List<CoinDto>>> = _coins
-
-    fun getCoinsFromRetrofit(){
-        viewModelScope.launch {
-            _coins.value = CoinApiResult.Loading()
-            try {
-                val coinsFromApi = withContext(Dispatchers.IO){
-                    coinRepostitory.getcoins()
-                }
-
-                _coins.value = CoinApiResult.Success(coinsFromApi)
-            } catch (e:Exception){
-                val coinResult = CoinApiResult.Error<List<CoinDto>>(e)
-            }
-        }
-    }
-}
-
-class CoinViewModelFactory(private val repostitory: CoinRepostitory): ViewModelProvider.Factory{
-    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return DetailsViewModel(repostitory)as T
-    }
-}
+class DetailsViewModel : ViewModel()
