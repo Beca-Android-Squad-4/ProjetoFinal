5c213e6aa39b25bee7105dfff198f6084846bc52
samuelbaldasso <samuelbaldasso93@gmail.com>
2022-05-24 11:23:18 -0300

    layout da tela de favoritos

--- app/src/main/java/com/example/projetofinalquad4/data/ICoinsClient.kt
+++ app/src/main/java/com/example/projetofinalquad4/data/ICoinsClient.kt
@@ -1,17 +1,12 @@
-// package com.example.projetofinalquad4.data
-//
-// import com.example.projetofinalquad4.data.remote.dto.CoinIcons
-// import com.example.projetofinalquad4.data.remote.dto.CoinTrocas
-// import retrofit2.http.GET
-// import retrofit2.http.Path
-//
-// interface ICoinsClient {
-//
+package com.example.projetofinalquad4.data
+
+interface ICoinsClient {
+
 //    @GET("/v1/exchanges")
 //    suspend fun getcoins(): List<CoinTrocas>
 //
 //    @GET("/v1/exchanges/icons/{iconSize}")
 //    suspend fun getCoinByIcon(@Path("coinId") coinId: String): CoinIcons
-// }
-// interface ICoinsClient
-//
+}
+//interface ICoinsClient
+
