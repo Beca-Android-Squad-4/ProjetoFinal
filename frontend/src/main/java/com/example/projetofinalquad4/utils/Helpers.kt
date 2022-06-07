168c80fc0b1386e646aab702abe23159f13f9284
tpaivafe <theylonfeitosa@gmail.com>
2022-06-07 15:24:14 -0300

    Format infoFragment + Helpers clean

--- frontend/src/main/java/com/example/projetofinalquad4/utils/Helpers.kt
+++ frontend/src/main/java/com/example/projetofinalquad4/utils/Helpers.kt
@@ -17,9 +17,6 @@ import kotlin.collections.ArrayList
 class Helpers {
 
     companion object {
-        fun ToastText(text: String, context: Context) {
-            Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
-        }
 
         fun FilterListQuery(text: String?, list: List<CoinItem>): MutableList<CoinItem> {
             var newList: MutableList<CoinItem> = ArrayList()
@@ -61,34 +58,13 @@ class Helpers {
             } else if (price.equals(0)) {
                 "Sem Preço"
             } else if (price > 9999999) {
-                val dec = DecimalFormat("##,###,###")
+                val dec = DecimalFormat("######.##")
                 var result = "$ " + dec.format(price)
                 result
             } else {
                 val dec = DecimalFormat("##,###.##")
                 "$ " + dec.format(price)
             }
-        }
-
-        fun formatPriceVolumeHoraCoin(price: Double): String {
-            // val teste = ((price / 10000000000000) * 1000) * 1000
-            val dec = DecimalFormat("##,###.##")
-            // return String.format("$ %.2f", price)
-            return dec.format(price)
-        }
-
-        fun formatPriceVolumeDiaCoin(price: Double): String {
-            // val teste = ((price / 1000000000000000) * 1000) * 1000
-            val dec = DecimalFormat("##,###.##")
-            // return String.format("$ %.2f", price)
-            return dec.format(price)
-        }
-
-        fun formatPriceVolumeMesCoin(price: Double): String {
-            // val teste = ((price / 1000000000000000000) * 100) * 1000
-            val dec = DecimalFormat("##,###.##")
-            // return String.format("$ %.2f", price)
-            return dec.format(price)
         }
     }
 }
