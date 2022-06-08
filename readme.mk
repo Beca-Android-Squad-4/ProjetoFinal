25c61912028869c8c9cc6d55d597c361a49b6875
Ana Claudia Gomes Souza <103140224+annecgs@users.noreply.github.com>
2022-06-08 15:32:19 -0300

    Update readme.mk

--- readme.mk
+++ readme.mk
@@ -1,3 +1,11 @@
+# Teste de acessibilidade com talkback
+<a href="https://www.loom.com/share/476d65a029a44d0280da62f76813dd81">Testes de acessibilidade com talkback</a>
+
+
+
+
+
+
 ### Agradecimento aos autores que criaram os vetores usados
 <a href="https://www.flaticon.com/br/icones-gratis/generico" title="genérico ícones">Genérico ícones criados por Freepik - Flaticon</a>
 <a href='https://www.freepik.com/vectors/404-page'>404 page vector created by pikisuperstar - www.freepik.com</a>
